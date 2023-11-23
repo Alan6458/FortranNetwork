31963f3bfaba26fb9d96db6bb45a794046b1cf8f
Alan Wu <alanwu0160@gmail.com>
2023-11-22 22:24:09 -0800

    Edited demos

--- Demos/Forward-Propagation/fNetFProp.f90
+++ Demos/Forward-Propagation/fNetFProp.f90
@@ -3,19 +3,22 @@ program fNetwork
     implicit none
     
     integer :: ln(2) = (/2, 3/)
-    character :: a = "s"
+    character (len = 1) :: a
     integer :: i
-    type(fNet) :: fn
+    type(fNetLayer), dimension(1) :: fn
     type(fNetOutLayer), dimension(size(ln)) :: res
+    a = "r"
     fn = initiateNetwork(ln, a)
-    fn%layer(1)%weights(1,:) = (/2, 3, 5/)
-    fn%layer(1)%weights(2,:) = (/6, 7, 4/)
-    fn%layer(1)%biases = (/2, 3, 4/)
+    fn(1)%weights(1,:) = (/2, 3, 5/)
+    fn(1)%weights(2,:) = (/6, 7, 4/)
+    fn(1)%biases = (/2, 3, 4/)
     do i = 1, 2
-        print *, fn%layer(1)%weights(i,:)
+        print *, fn(1)%weights(i,:)
     end do
     print *, ""
-    print *, fn%layer(1)%biases
+    print *, fn(1)%biases
+    print *, fn(1)%activation
+    print *, fn(1)%layerSize
     print *, ""
     res = forwardProp(fn, (/3, 4/))
     do i = 1, 2
--- Demos/Initiation/fNetInit.f90
+++ Demos/Initiation/fNetInit.f90
@@ -3,16 +3,19 @@ program fNetwork
     implicit none
     
     integer :: ln(3) = (/2, 4, 3/)
-    character :: a = "s"
+    character (len = 2) :: a
     integer :: i, j
-    type(fNet) :: fn
+    type(fNetLayer), dimension(2) :: fn
+    a = "ss"
     fn = initiateNetwork(ln, a)
     do i = 1, 2
         do j = 1, ln(i)
-            print *, fn%layer(i)%weights(j,:)
+            print *, fn(i)%weights(j,:)
         end do
         print *, ""
-        print *, fn%layer(i)%biases
+        print *, fn(i)%biases
+        print *, fn(i)%activation
+        print *, fn(i)%layerSize
         print *, ""
     end do
 end program fNetwork
